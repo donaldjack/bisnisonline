9bd94ab9466f7fa0409b1c5ffb16685c68ec7ec6
donaldjack <39451962+donaldjack@users.noreply.github.com>
2019-08-21 20:55:00 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2019/07/samsung-galaxy-m21-m31-and-m41-key.html"}
 if(x=="2"){xxx="/2019/07/facebook-warned-body-of-workers-now-not.html"}
 if(x=="3"){xxx="/2019/07/apple-stops-contractors-from-listening.html"}
 if(x=="4"){xxx="/2019/07/samsung-galaxy-a90-5g-spotted-on-wifi.html"}
-if(x=="5"){xxx="/flipkart-ideas-to-help-customers-on.html"}a_to_ve[i].href="https://www.bisnisonlinegratis.me"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2019/07/flipkart-ideas-to-help-customers-on.html"}a_to_ve[i].href="https://www.bisnisonlinegratis.me"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
